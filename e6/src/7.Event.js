bd1e3f0866664ffef160ccfc15ff0ec1f273e1ca
ice <1597834867@qq.com>
2024-05-02 20:12:53 +0800

    feat: event

---
+++ e6/src/7.Event.js
@@ -0,0 +1,22 @@
+const { ethers } = require("ethers");
+
+const API = require("./abi/WETH.json");
+const getProviders = require("./utils/getProviders");
+const { WETH_CONTRACH, ADDRESS } = require("../config");
+
+const { provider_sepolia } = getProviders();
+
+const contractWETH = new ethers.Contract(WETH_CONTRACH, API, provider_sepolia);
+
+async function main() {
+  const block = await provider_sepolia.getBlockNumber();
+  const [event] = await contractWETH.queryFilter(
+    "Transfer",
+    block - 1000,
+    block
+  );
+  const [from, to, amount] = event.args;
+  console.log(`from: ${from} -> to: ${to}`, ethers.formatEther(amount));
+}
+
+main();
